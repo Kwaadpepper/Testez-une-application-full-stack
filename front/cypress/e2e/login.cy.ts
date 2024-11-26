1f57328d25d7e45e9b57abacc88305d44f2d803d
Jérémy Munsch <github@jeremydev.ovh>
2024-11-26 20:32:37 +0100

    test(frontend): :white_check_mark: Front: Add Cypress login tests

--- front/cypress/e2e/login.cy.ts
+++ front/cypress/e2e/login.cy.ts
@@ -1,27 +1,22 @@
 describe('Login spec', () => {
   it('Login successfull', () => {
+    cy.login();
+
+    cy.url().should('include', '/sessions')
+    cy.get('.mat-card-title').should("contain", "Rentals available")
+    cy.get('.mat-toolbar > .ng-star-inserted > :nth-child(3)').should("contain", "Logout")
+  })
+
+  it('Login Failure', () => {
     cy.visit('/login')
 
     cy.intercept('POST', '/api/auth/login', {
-      body: {
-        id: 1,
-        username: 'userName',
-        firstName: 'firstName',
-        lastName: 'lastName',
-        admin: true
-      },
+      statusCode: 400,
     })
-
-    cy.intercept(
-      {
-        method: 'GET',
-        url: '/api/session',
-      },
-      []).as('session')
 
     cy.get('input[formControlName=email]').type("yoga@studio.com")
     cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
 
-    cy.url().should('include', '/sessions')
+    cy.get('.error').should('contain', 'An error occurred')
   })
 });
