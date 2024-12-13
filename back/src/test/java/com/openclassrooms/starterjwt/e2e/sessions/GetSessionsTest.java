45b46e1d3d5906f19ebf6618627dffc0d47f5e10
Jérémy Munsch <github@jeremydev.ovh>
2024-12-13 14:18:42 +0100

    test(backend): Add E2E getSessions test

---
+++ back/src/test/java/com/openclassrooms/starterjwt/e2e/sessions/GetSessionsTest.java
@@ -0,0 +1,82 @@
+package com.openclassrooms.starterjwt.e2e.sessions;
+
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.hasEntry;
+import static org.hamcrest.Matchers.hasItem;
+
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
+import org.json.JSONException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.openclassrooms.starterjwt.models.Session;
+import com.openclassrooms.starterjwt.security.jwt.JwtUtils;
+import com.openclassrooms.starterjwt.services.SessionService;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class GetSessionsTest {
+
+    private static final String BASE_URL = "/api/session";
+
+    @Autowired
+    private SessionService sessionService;
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @LocalServerPort
+    private int port;
+
+    String jwt;
+
+    @BeforeEach
+    public void init() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = this.port;
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken("yoga@studio.com", "password"));
+        jwt = jwtUtils.generateJwtToken(authentication);
+    }
+
+    @Test
+    void canGetSessionsList() {
+        List<Session> sessionList = sessionService.findAll();
+        Assertions.assertThat(sessionList)
+                .size().isGreaterThan(0);
+    }
+
+    @Test
+    void canGetSessionsListFromEndpoint() throws JSONException {
+        RestAssured
+                .given()
+                .header("Authorization", "Bearer " + jwt)
+                .contentType(ContentType.JSON)
+                .log().uri()
+                .log().method()
+                .when().get(BASE_URL)
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("", hasItem(allOf(
+                        hasEntry("name", "Session name"),
+                        hasEntry("description", "Session description"))));
+    }
+}
