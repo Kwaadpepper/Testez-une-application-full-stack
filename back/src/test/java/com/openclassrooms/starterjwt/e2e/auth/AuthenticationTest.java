05d5be51ee43598f54530c073b3bdef3035d4754
Jérémy Munsch <github@jeremydev.ovh>
2024-12-13 13:50:17 +0100

    test(backend): :white_check_mark: E2E Test Authentication

---
+++ back/src/test/java/com/openclassrooms/starterjwt/e2e/auth/AuthenticationTest.java
@@ -0,0 +1,65 @@
+package com.openclassrooms.starterjwt.e2e.auth;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.openclassrooms.starterjwt.models.User;
+import com.openclassrooms.starterjwt.services.UserService;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AuthenticationTest {
+
+    private static final String BASE_URL = "/api/auth";
+
+    @Autowired
+    private UserService usersService;
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    public void init() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = this.port;
+    }
+
+    @Test
+    void canFetchUserByUsername() {
+        User user = usersService.findById(1L);
+        assertEquals(user.getId(), 1L);
+        assertEquals(user.getEmail(), "yoga@studio.com");
+    }
+
+    @Test
+    void canAuthenticateUser() throws JSONException {
+        RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .body(
+                        (new JSONObject())
+                                .put("email", "yoga@studio.com")
+                                .put("password", "password")
+                                .toString())
+                .log().uri()
+                .log().method()
+                .when().post(BASE_URL + "/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("username", equalTo("yoga@studio.com"));
+    }
+}
---
+++ back/src/test/resources/schema.sql
@@ -0,0 +1,49 @@
+DROP TABLE IF EXISTS PARTICIPATE;
+DROP TABLE IF EXISTS SESSIONS;
+DROP TABLE IF EXISTS USERS;
+DROP TABLE IF EXISTS TEACHERS;
+
+CREATE TABLE USERS (
+  id int NOT NULL,
+  last_name varchar(40) DEFAULT NULL,
+  first_name varchar(40) DEFAULT NULL,
+  admin tinyint NOT NULL DEFAULT 0,
+  email varchar(255) DEFAULT NULL,
+  password varchar(255) DEFAULT NULL,
+  created_at timestamp(0) NULL DEFAULT current_timestamp(),
+  updated_at datetime DEFAULT current_timestamp() /* ON UPDATE current_timestamp() */,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE TEACHERS (
+  id int NOT NULL,
+  last_name varchar(40) DEFAULT NULL,
+  first_name varchar(40) DEFAULT NULL,
+  created_at timestamp(0) NULL DEFAULT current_timestamp(),
+  updated_at datetime DEFAULT current_timestamp() /* ON UPDATE current_timestamp() */,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE SESSIONS (
+  id int NOT NULL,
+  name varchar(50) DEFAULT NULL,
+  description varchar(2000) DEFAULT NULL,
+  date timestamp(0) NULL DEFAULT NULL,
+  teacher_id int DEFAULT NULL,
+  created_at timestamp(0) NULL DEFAULT current_timestamp(),
+  updated_at datetime DEFAULT current_timestamp() /* ON UPDATE current_timestamp() */,
+  PRIMARY KEY (id),
+  CONSTRAINT SESSIONS_ibfk_1 FOREIGN KEY (teacher_id) REFERENCES TEACHERS (id)
+);
+
+CREATE INDEX teacher_id ON SESSIONS (teacher_id);
+
+CREATE TABLE PARTICIPATE (
+  user_id int DEFAULT NULL,
+  session_id int DEFAULT NULL,
+  CONSTRAINT PARTICIPATE_ibfk_1 FOREIGN KEY (user_id) REFERENCES USERS (id),
+  CONSTRAINT PARTICIPATE_ibfk_2 FOREIGN KEY (session_id) REFERENCES SESSIONS (id)
+);
+
+CREATE INDEX user_id ON PARTICIPATE (user_id);
+CREATE INDEX session_id ON PARTICIPATE (session_id);
