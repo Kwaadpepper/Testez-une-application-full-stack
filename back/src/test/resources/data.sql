68c5aaee78ffe94bd45f51c13f22c5212f586acf
Jérémy Munsch <github@jeremydev.ovh>
2024-12-16 22:50:51 +0100

    test(backend): update data.sql for integration tests

--- back/src/test/resources/data.sql
+++ back/src/test/resources/data.sql
@@ -4,7 +4,8 @@ INSERT INTO TEACHERS (id, last_name, first_name, created_at, updated_at) VALUES
 
 INSERT INTO USERS (id, last_name, first_name, admin, email, password, created_at, updated_at) VALUES
 	(1, 'Admin', 'Admin', 1, 'yoga@studio.com', '$2a$10$hHtCFfR5T/CLA7Xq5VbV/euvibwHafiNhPu5PG1aDoNENlCdx800S', '2024-11-26 18:10:14', '2024-11-26 18:10:14'),
-	(2, 'user', 'example', 0, 'user@example.net', '$2a$10$hHtCFfR5T/CLA7Xq5VbV/euvibwHafiNhPu5PG1aDoNENlCdx800S', '2024-11-26 19:12:40', '2024-12-13 11:24:35');
+	(2, 'user', 'example', 0, 'user@example.net', '$2a$10$hHtCFfR5T/CLA7Xq5VbV/euvibwHafiNhPu5PG1aDoNENlCdx800S', '2024-11-26 19:12:40', '2024-12-13 11:24:35'),
+	(3, 'another user', 'example', 0, 'another.user@example.net', '$2a$10$hHtCFfR5T/CLA7Xq5VbV/euvibwHafiNhPu5PG1aDoNENlCdx800S', '2024-11-26 19:12:40', '2024-12-13 11:24:35');
 
 INSERT INTO SESSIONS (id, name, description, date, teacher_id, created_at, updated_at) VALUES
 	(1, 'Session name', 'Session description', '2023-10-27 02:00:00', 1, '2024-11-26 19:21:35', '2024-12-13 11:24:17');
