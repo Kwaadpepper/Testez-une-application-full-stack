9d2d7943003354fd2bb8c3d4599066fb40af40c2
Jérémy Munsch <github@jeremydev.ovh>
2024-12-20 14:23:48 +0100

    test(frontend): Add Unit test teacher.service

--- front/src/app/services/teacher.service.spec.ts
+++ front/src/app/services/teacher.service.spec.ts
@@ -1,22 +1,56 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
+import { Observable } from 'rxjs';
 import { TeacherService } from './teacher.service';
 
 describe('TeacherService', () => {
-  let service: TeacherService;
+  const pathService = 'api/teacher'
+  let teacherService: TeacherService
+  let httpClient: HttpClient
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports:[
         HttpClientModule
       ]
-    });
-    service = TestBed.inject(TeacherService);
-  });
+    })
+
+    httpClient = TestBed.inject(HttpClient)
+    teacherService = TestBed.inject(TeacherService)
+  })
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+    expect(teacherService).toBeTruthy();
+  })
+
+  it('should get all', () => {
+    // Arrange
+    jest.spyOn(httpClient, 'get')
+
+    // Act
+    const output = teacherService.all()
+
+    // Assert
+    expect(output).toBeDefined()
+    expect(output).toBeInstanceOf(Observable)
+    expect(httpClient.get).toHaveBeenCalledTimes(1)
+    expect(httpClient.get).toHaveBeenCalledWith(pathService)
+  })
+
+  it('should get by id', () => {
+    // Arrange
+    const teacherId = 1;
+    jest.spyOn(httpClient, 'get')
+
+    // Act
+    const output = teacherService.detail(String(teacherId))
+
+    // Assert
+    expect(output).toBeDefined()
+    expect(output).toBeInstanceOf(Observable)
+    expect(httpClient.get).toHaveBeenCalledTimes(1)
+    expect(httpClient.get).toHaveBeenCalledWith(`${pathService}/${teacherId}`)
+  })
+})
