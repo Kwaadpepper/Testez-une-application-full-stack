d89a80e91f69f388224c2eae71c1e32924119339
Jérémy Munsch <github@jeremydev.ovh>
2024-12-20 14:41:08 +0100

    test(frontend): Add Unit test session.service

--- front/src/app/services/session.service.spec.ts
+++ front/src/app/services/session.service.spec.ts
@@ -1,17 +1,67 @@
 import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
+import { SessionInformation } from '../interfaces/sessionInformation.interface';
 import { SessionService } from './session.service';
 
 describe('SessionService', () => {
-  let service: SessionService;
+  let sessionService: SessionService
+
+  const mockSessionInformation: SessionInformation = {
+    token: "token",
+    type: "type",
+    id: 1,
+    username: 'username',
+    firstName: 'firstName',
+    lastName: 'lastName',
+    admin: false
+  }
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(SessionService);
-  });
+
+    sessionService = TestBed.inject(SessionService);
+  })
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+    expect(sessionService).toBeTruthy();
+  })
+
+  it('should log in', () => {
+    // Arrange
+    let observedIsLogged = false
+    sessionService.isLogged = observedIsLogged
+    sessionService.$isLogged().subscribe({
+      next(v) {
+        observedIsLogged = v
+      }
+    })
+
+    // Act
+    sessionService.logIn(mockSessionInformation)
+
+    // Assert
+    expect(sessionService.isLogged).toBeTruthy()
+    expect(sessionService.sessionInformation).toBe(mockSessionInformation)
+    expect(observedIsLogged).toBeTruthy()
+  })
+
+  it('should log out', () => {
+    // Arrange
+    let observedIsLogged = true
+    sessionService.isLogged = observedIsLogged
+    sessionService.$isLogged().subscribe({
+      next(v) {
+        observedIsLogged = v
+      }
+    })
+
+    // Act
+    sessionService.logOut()
+
+    // Assert
+    expect(sessionService.isLogged).toBeFalsy()
+    expect(sessionService.sessionInformation).toBeUndefined()
+    expect(observedIsLogged).toBeFalsy()
+  })
+})
