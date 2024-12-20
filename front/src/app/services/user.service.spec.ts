ab1e1782a34ca3bb10d0ac0cccc2ec06b8c8a19b
Jérémy Munsch <github@jeremydev.ovh>
2024-12-20 13:40:16 +0100

    test(frontend): Add Unit test user.service

--- front/src/app/services/user.service.spec.ts
+++ front/src/app/services/user.service.spec.ts
@@ -1,22 +1,57 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
+import { Observable } from 'rxjs';
 import { UserService } from './user.service';
 
 describe('UserService', () => {
-  let service: UserService;
+  const pathService = 'api/user'
+  let userService: UserService
+  let httpClient: HttpClient
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports:[
         HttpClientModule
       ]
-    });
-    service = TestBed.inject(UserService);
+    })
+
+    httpClient = TestBed.inject(HttpClient)
+    userService = TestBed.inject(UserService)
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+    expect(userService).toBeTruthy();
+  })
+
+  it('should get by id', () => {
+    // Arrange
+    const userId = 1;
+    jest.spyOn(httpClient, 'get')
+
+    // Act
+    const output = userService.getById(String(userId))
+
+    // Assert
+    expect(output).toBeDefined()
+    expect(output).toBeInstanceOf(Observable)
+    expect(httpClient.get).toHaveBeenCalledTimes(1)
+    expect(httpClient.get).toHaveBeenCalledWith(`${pathService}/${userId}`)
+  })
+
+  it('should delete by id', () => {
+    // Arrange
+    const userId = 1;
+    jest.spyOn(httpClient, 'delete')
+
+    // Act
+    const output = userService.delete(String(userId))
+
+    // Assert
+    expect(output).toBeDefined()
+    expect(output).toBeInstanceOf(Observable)
+    expect(httpClient.delete).toHaveBeenCalledTimes(1)
+    expect(httpClient.delete).toHaveBeenCalledWith(`${pathService}/${userId}`)
+  })
+})
