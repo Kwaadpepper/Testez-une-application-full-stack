e7127c56dea8e2a49776d308c39555c868f2fc35
Jérémy Munsch <github@jeremydev.ovh>
2024-12-20 15:51:42 +0100

    test(frontend): Add Unit test app.component

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -1,29 +1,56 @@
-import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterTestingModule } from '@angular/router/testing';
 import { expect } from '@jest/globals';
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Router } from '@angular/router';
 import { AppComponent } from './app.component';
+import { SessionService } from './services/session.service';
 
 
 describe('AppComponent', () => {
+  let sessionService: SessionService
+  let router: Router
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
-        HttpClientModule,
+        HttpClientTestingModule,
         MatToolbarModule
       ],
       declarations: [
         AppComponent
       ],
-    }).compileComponents();
-  });
+    }).compileComponents()
+
+    sessionService = TestBed.inject(SessionService)
+    router = TestBed.inject(Router)
+  })
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-});
+    // Arrange
+    const fixture = TestBed.createComponent(AppComponent)
+    const app = fixture.componentInstance
+
+    // Assert
+    expect(app).toBeTruthy()
+  })
+
+  it('should logout from the app', () => {
+    // Arrange
+    const fixture = TestBed.createComponent(AppComponent)
+    const app = fixture.componentInstance
+    jest.spyOn(sessionService, 'logOut')
+    jest.spyOn(router, 'navigate')
+
+    // Act
+    app.logout()
+
+    // Assert
+    expect(app).toBeTruthy()
+    expect(sessionService.logOut).toHaveBeenCalledTimes(1)
+    expect(router.navigate).toHaveBeenCalledWith([''])
+  })
+})
