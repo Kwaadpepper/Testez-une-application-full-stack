9f9f2cc0ccee8c67615d7319f56a591dd0386ea3
Jérémy Munsch <github@jeremydev.ovh>
2024-12-22 18:28:44 +0100

    test(frontend): Add Unit register.component

--- front/src/app/features/auth/components/register/register.component.spec.ts
+++ front/src/app/features/auth/components/register/register.component.spec.ts
@@ -1,18 +1,25 @@
 import { HttpClientModule } from '@angular/common/http';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { expect } from '@jest/globals';
+import { Router } from '@angular/router';
+import { expect, jest } from '@jest/globals';
 
+import { Subject } from 'rxjs';
+import { SessionService } from 'src/app/services/session.service';
+import { AuthService } from '../../services/auth.service';
 import { RegisterComponent } from './register.component';
 
 describe('RegisterComponent', () => {
-  let component: RegisterComponent;
-  let fixture: ComponentFixture<RegisterComponent>;
+  let registerComponent: RegisterComponent
+  let fixture: ComponentFixture<RegisterComponent>
+  let router: Router
+  let sessionService: SessionService
+  let authService:AuthService
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -20,21 +27,65 @@ describe('RegisterComponent', () => {
       imports: [
         BrowserAnimationsModule,
         HttpClientModule,
-        ReactiveFormsModule,  
+        ReactiveFormsModule,
         MatCardModule,
         MatFormFieldModule,
         MatIconModule,
         MatInputModule
       ]
     })
-      .compileComponents();
+      .compileComponents()
 
-    fixture = TestBed.createComponent(RegisterComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    router = TestBed.inject(Router)
+    sessionService = TestBed.inject(SessionService)
+    authService = TestBed.inject(AuthService)
+
+    fixture = TestBed.createComponent(RegisterComponent)
+    registerComponent = fixture.componentInstance
+    fixture.detectChanges()
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(registerComponent).toBeTruthy()
+  })
+
+  it('should register successfully',  fakeAsync(() => {
+    // -- Arrange
+    registerComponent.onError = false
+    const authServiceRegister = new Subject<void>()
+    const authServiceRegister$ = authServiceRegister.asObservable()
+
+    authService.register = jest.fn(() => authServiceRegister$)
+    jest.spyOn(router, 'navigate').mockImplementation(() => Promise.resolve(true))
+
+    // -- Act
+    registerComponent.submit()
+    authServiceRegister.next()
+    tick(1000)
+
+    // Assert
+    expect(authService.register).toHaveBeenCalled()
+    expect(registerComponent.onError).toBeFalsy()
+    expect(router.navigate).toHaveBeenCalledWith(['/login'])
+  }))
+
+  it('should fail to register gracefully', fakeAsync(() => {
+    // -- Arrange
+    registerComponent.onError = false
+    const authServiceRegister = new Subject<void>()
+    const authServiceRegister$ = authServiceRegister.asObservable()
+
+    authService.register = jest.fn(() => authServiceRegister$)
+    jest.spyOn(router, 'navigate')
+
+    // -- Act
+    registerComponent.submit()
+    authServiceRegister.error(new Error())
+    tick(1000)
+
+    // Assert
+    expect(authService.register).toHaveBeenCalled()
+    expect(registerComponent.onError).toBeTruthy()
+    expect(router.navigate).not.toHaveBeenCalled()
+  }))
+})
