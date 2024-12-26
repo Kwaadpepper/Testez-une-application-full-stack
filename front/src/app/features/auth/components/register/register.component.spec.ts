f9f6590b67e2f6a39003ea80b2b3ac93b1a2149f
Jérémy Munsch <github@jeremydev.ovh>
2024-12-26 16:00:45 +0100

    test(frontend): Add integration tests to register.component

--- front/src/app/features/auth/components/register/register.component.spec.ts
+++ front/src/app/features/auth/components/register/register.component.spec.ts
@@ -1,4 +1,3 @@
-import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
@@ -9,6 +8,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Router } from '@angular/router';
 import { expect, jest } from '@jest/globals';
 
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { Subject } from 'rxjs';
 import { SessionService } from 'src/app/services/session.service';
 import { AuthService } from '../../services/auth.service';
@@ -20,13 +20,14 @@ describe('RegisterComponent', () => {
   let router: Router
   let sessionService: SessionService
   let authService:AuthService
+  let httpTestingController:HttpTestingController
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [RegisterComponent],
       imports: [
         BrowserAnimationsModule,
-        HttpClientModule,
+        HttpClientTestingModule,
         ReactiveFormsModule,
         MatCardModule,
         MatFormFieldModule,
@@ -35,6 +36,8 @@ describe('RegisterComponent', () => {
       ]
     })
       .compileComponents()
+
+    httpTestingController = TestBed.inject(HttpTestingController)
 
     router = TestBed.inject(Router)
     sessionService = TestBed.inject(SessionService)
@@ -88,4 +91,90 @@ describe('RegisterComponent', () => {
     expect(registerComponent.onError).toBeTruthy()
     expect(router.navigate).not.toHaveBeenCalled()
   }))
+
+  it('should register gracefully IT', () => {
+    // -- Arrange
+    const divElement: HTMLElement = fixture.nativeElement
+    const firstNameInput = divElement.querySelectorAll('input').item(0) as HTMLInputElement | null
+    const lastNameInput = divElement.querySelectorAll('input').item(1) as HTMLInputElement | null
+    const emailNameInput = divElement.querySelectorAll('input').item(2) as HTMLInputElement | null
+    const passwordNameInput = divElement.querySelectorAll('input').item(3) as HTMLInputElement | null
+    const submitButton = divElement.querySelector('button[type="submit"]') as HTMLButtonElement | null
+    jest.spyOn(authService, 'register')
+    jest.spyOn(router, 'navigate')
+
+    // -- Act
+    firstNameInput!.value = 'firstName'
+    firstNameInput!.dispatchEvent(new Event('input'))
+    lastNameInput!.value = 'lastName'
+    lastNameInput!.dispatchEvent(new Event('input'))
+    emailNameInput!.value = 'user@example.net'
+    emailNameInput!.dispatchEvent(new Event('input'))
+    passwordNameInput!.value = 'superpassword'
+    passwordNameInput!.dispatchEvent(new Event('input'))
+    fixture.detectChanges()
+    submitButton!.click()
+
+    // Assert
+    const req = httpTestingController.expectOne({
+      url: 'api/auth/register',
+      method: 'POST'
+    });
+    req.flush({}, {status: 200, statusText: ''})
+    expect(authService.register).toHaveBeenCalled()
+    expect(registerComponent.onError).toBeFalsy()
+    httpTestingController.verify();
+    expect(router.navigate).toHaveBeenCalledWith(['/login'])
+  })
+
+  it('should fail to register gracefully IT', fakeAsync(() => {
+    // -- Arrange
+    const divElement: HTMLElement = fixture.nativeElement
+    const firstNameInput = divElement.querySelectorAll('input').item(0) as HTMLInputElement | null
+    const lastNameInput = divElement.querySelectorAll('input').item(1) as HTMLInputElement | null
+    const emailNameInput = divElement.querySelectorAll('input').item(2) as HTMLInputElement | null
+    const passwordNameInput = divElement.querySelectorAll('input').item(3) as HTMLInputElement | null
+    const submitButton = divElement.querySelector('button[type="submit"]') as HTMLButtonElement | null
+    jest.spyOn(authService, 'register')
+    jest.spyOn(router, 'navigate')
+
+    // -- Act
+    firstNameInput!.value = 'firstName'
+    firstNameInput!.dispatchEvent(new Event('input'))
+    lastNameInput!.value = 'lastName'
+    lastNameInput!.dispatchEvent(new Event('input'))
+    emailNameInput!.value = 'user@example.net'
+    emailNameInput!.dispatchEvent(new Event('input'))
+    passwordNameInput!.value = 'superpassword'
+    passwordNameInput!.dispatchEvent(new Event('input'))
+    fixture.detectChanges()
+    submitButton!.click()
+
+    // Assert
+    const req = httpTestingController.expectOne({
+      url: 'api/auth/register',
+      method: 'POST'
+    });
+    req.flush({}, {status: 400, statusText: ''})
+    expect(authService.register).toHaveBeenCalled()
+    expect(registerComponent.onError).toBeTruthy()
+    httpTestingController.verify()
+    tick(4000)
+    fixture.detectChanges()
+    expect(divElement.querySelector('span.error')).toBeTruthy()
+    expect(divElement.querySelector('span.error')!.textContent).toBe('An error occurred')
+  }))
+
+    it('should require values to register IT', async () => {
+      // -- Arrange
+      const divElement: HTMLElement = fixture.nativeElement
+      const submitButton = divElement.querySelector('button[type="submit"]') as HTMLButtonElement | null
+
+      // -- Act
+
+
+      // Assert
+      expect(registerComponent.form.invalid).toBeTruthy()
+      expect(submitButton!.disabled).toBeTruthy()
+    })
 })
